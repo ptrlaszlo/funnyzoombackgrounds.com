5c473a294314a593092e08fd44d04d1750260164
Peter Laszlo <peter.laszlo@tnt-digital.com>
2020-04-02 16:55:09 +0200

    more images

--- generate.sh
+++ generate.sh
@@ -1,13 +1,29 @@
 #!/usr/bin/env amm
 
 val pictures = List(
+  ("distracted.jpg", "Distracted Boyfriend Meme", "https://twitter.com/BeeShellll/status/1245030591386566656"),
+  ("thisisfine.jpg", "This Is Fine Meme", "https://twitter.com/BeeShellll/status/1245030701994586113"),
+  ("toilet_paper.jpg", "Toilet Paper", "https://twitter.com/BeeShellll/status/1245030825491562499"),
+  
+  ("tiger_king.jpg", "The Tiger King", "https://twitter.com/BeeShellll/status/1245030825491562499"),
+  ("office.jpg", "Office", "https://twitter.com/BeeShellll/status/1245030591386566656"),
+  ("bbc.jpg", "BBC guy", "https://twitter.com/BDStanley/status/1245071576594350087"),
+
+  ("club_penguin.jpg", "Club Penguin", "https://twitter.com/BeeShellll/status/1245030591386566656"),
+  ("sponge.jpg", "Sponge Bob", "https://twitter.com/BeeShellll/status/1245030701994586113"),
+  ("icarly.jpg", "iCarly's bedroom", "https://twitter.com/BeeShellll/status/1245030591386566656"),
+  
+  ("phineas.jpg", "Phineas and Ferb", "https://twitter.com/BeeShellll/status/1245030701994586113"),
+  ("monster_inc.jpg", "Monster Inc.", "https://twitter.com/BeeShellll/status/1245030825491562499"),
+  ("friends.jpg", "Friends", "https://twitter.com/chanelizette/status/1245147224272588800"),
+
   ("ISS_cupola.jpg", "The ISS cupola", "https://twitter.com/AeroDork/status/1243354801225846784"),
   ("wright.jpg", "The Wright Flyer", "https://twitter.com/AeroDork/status/1243355008852062208"),
   ("c130.jpg", "C-130 off ramp", "https://twitter.com/AeroDork/status/1243355151185797120"),
 
   ("apollo-17.jpg", "Apollo 17", "https://twitter.com/AeroDork/status/1243355243347423237"),
   ("kim.jpg", "Rocket launch", "https://twitter.com/AeroDork/status/1243355334418157568"),
-  ("hilton-lobby.jpg", "The Hilton lobby", "https://twitter.com/AeroDork/status/1243358086795915264")
+  ("hilton-lobby.jpg", "The Hilton lobby", "https://twitter.com/AeroDork/status/1243358086795915264"),
 )
 
 import java.io._
@@ -52,7 +68,7 @@ pictures.grouped(3).foreach { rowPics =>
     <figure class="image is-4by3">
       <a href="assets/img/$img"><img src="assets/img/$img"></a>
     </figure>
-    <a class="is-pulled-right" href="$url" target="_blank">source</a>
+    <a class="is-pulled-right is-size-7" href="$url" target="_blank">source</a>
   </article>
 </div>
     """
@@ -62,6 +78,24 @@ pictures.grouped(3).foreach { rowPics =>
   pw.write(s"""<div class="tile is-ancestor">${picsHtml.mkString("")}</div>""")
 }
 
-pw.write("""</section></body></html>""")
+pw.write("""</section>""")
+
+pw.write("""
+<section class="section">
+  <div class="container">
+    <h2 class="title">Zoom Virtual backgrounds</h2>
+    <p>
+      The official step-by-step guide to set Zoom Virtual backgrounds is <a href="https://support.zoom.us/hc/en-us/articles/210707503-Virtual-Background">here</a>
+    </p>
+  </div>
+</section>
+<footer class="footer">
+<div class="content has-text-centered">
+  <p>Built by <a href="https://twitter.com/ptrlaszlo">Peter László</a>. No affiliation with or endorsement by Zoom Video Communications, Inc. is intended or implied.</p>
+</div>
+</footer>
+""")
+
+pw.write("""</body></html>""")
 
 pw.close
